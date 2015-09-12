d50153604457227133b0d98eb356b94fb1e351af
Henrique Moco <hmoco94@vt.edu>
2015-09-12 13:00:30 -0400

    Add no pitch messafe

--- client/templates/pitches/pitches_page.js
+++ client/templates/pitches/pitches_page.js
@@ -1,5 +1,12 @@
 Template.pitches_page.helpers({
   pitch: function () {
     return Pitch.find({claimed_by: ''});
+  },
+  no_pitches: function() {
+  	var pitches = Pitch.find({claimed_by: ''}).fetch().length;
+  	if (pitches) {
+  		return false
+  	}
+  	return true
   }
 });
