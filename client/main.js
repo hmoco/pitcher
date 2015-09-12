44165c46e322246512c7007713c902f03398bfa6
Henrique Moco <hmoco94@vt.edu>
2015-09-12 11:11:07 -0400

    delete unnused

--- client/main.js
+++ client/main.js
@@ -5,26 +5,4 @@ Template.body.helpers({
 });
 
 Template.body.events({
-  "submit .new-pitch": function (event) {
-    event.preventDefault();
-
-    var title = event.target.title.value;
-    var description = event.target.description.value;
-
-    Pitch.insert({
-      claimed_by: '',
-      pitched_by: 'current_user',
-      title: title,
-      description: description,
-    });
-
-    event.target.title.value = "";
-    event.target.description.value = "";
-
-  },
-  "click .claim": function(event) {
-    event.preventDefault();
-    id = this._id;
-    Pitch.update(id, {$set: {claimed_by: 'current_user'}});
-  }
 });
